9661b1afb66eb9245e8bcbf7783c507f5992e1d8
Mohammad Khairi <mohammadkhairibinali@gmail.com>
2019-05-27 11:48:37 +0800

    change APIKeyValidator => APITokenKeyValidator in index.js

--- index.js
+++ index.js
@@ -3,11 +3,11 @@
 const BuildResponse = require('./src/middlewares/build_response');
 const HandleRequestUsingAsync = require('./src/middlewares/handle_request_using_async');
 const RegisterHTTPErrorCodeHandler = require('./src/middlewares/register_http_error_code_handler');
-const APIKeyValidator = require('./src/middlewares/api_key_validator');
+const APITokenKeyValidator = require('./src/middlewares/api_token_key_validator');
 
 module.exports = {
     BuildResponse,
     HandleRequestUsingAsync,
     RegisterHTTPErrorCodeHandler,
-    APIKeyValidator
+    APITokenKeyValidator
 }
